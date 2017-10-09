0f7feeb752d0bf56ca1b53b82d27b8b65db8b09b
Michael O'Donnell <mo14776@my.bristol.ac.uk>
2017-10-09 22:08:35 +0100

    Created maths files, for storing generetic mathematical functions.

---
+++ src/maths_functions.h
@@ -0,0 +1,16 @@
+/*********************
+* Date of creation 09/10/2017
+* Author: Michael O'Donnell
+* Contact: mo14776@my.bristol.ac.uk
+* Other Authors: N/A
+**************************************
+* History
+*
+**************************************/
+
+#ifndef _MATHS_FUNCTIONS_H
+#define _MATHS_FUNCTIONS_H
+#include <stdlib.h>
+
+int kronecker_delta(int i, int k);
+#endif  // _MATHS_FUNCTIONS_H
