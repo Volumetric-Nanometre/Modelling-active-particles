5549d8ef93e2ccf3191c0bc1a5f7ecce352c4e57
Michael O'Donnell <mo14776@my.bristol.ac.uk>
2017-10-10 21:36:30 +0100

    Added particleVariables to prototypes

--- src/diffusionmatrix.h
+++ src/diffusionmatrix.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-double *diffusion_matrix_creation(int numberOfParticles, double temperature, double viscosity, double radius);
+double *diffusion_matrix_creation(int numberOfParticles, particleVariables *particles, double temperature, double viscosity, double radius)
 
-void oseen_tensor_creation(double *oseenMatrix, double temperature, double viscosity, double radius, int i, int j);
+void oseen_tensor_creation(double *oseenMatrix, particleVariables *particles, double temperature, double viscosity, double radius, int i, int j)
 
 #endif //_DIFFUSIONMATRIX_H
