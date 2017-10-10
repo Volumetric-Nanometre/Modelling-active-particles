718109b1312bf0cf5c4deb392846a4032af61379
Michael O'Donnell <mo14776@my.bristol.ac.uk>
2017-10-10 21:27:22 +0100

    Added function prototypes

--- src/diffusionmatrix.h
+++ src/diffusionmatrix.h
@@ -1,3 +1,10 @@
 #ifndef _DIFFUSIONMATRIX_H
 #define _DIFFUSIONMATRIX_H
+
+#include <stdio.h>
+
+double *diffusion_matrix_creation(int numberOfParticles, double temperature, double viscosity, double radius);
+
+void oseen_tensor_creation(double *oseenMatrix, double temperature, double viscosity, double radius, int i, int j);
+
 #endif //_DIFFUSIONMATRIX_H
