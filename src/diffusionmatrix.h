00acc02c084a772e0eb4372665bed3c6c15d9d18
Michael O'Donnell <mo14776@my.bristol.ac.uk>
2017-10-11 15:16:13 +0100

    Added rotational tensor support

--- src/diffusionmatrix.h
+++ src/diffusionmatrix.h
@@ -6,6 +6,9 @@
 
 double *diffusion_matrix_creation(int numberOfParticles, particleVariables *particles, double temperature, double viscosity, double radius);
 
-void translational_tensor_creation(double *oseenMatrix, particleVariables *particles, double temperature, double viscosity, double radius, int i, int j);
+void translational_tensor_creation(double *tempMatrix, particleVariables *particles, double temperature, double viscosity, double radius, int i, int j);
+
+void rotational_tensor_creation(double *tempMatrix, particleVariables *particles, double temperature, double viscosity, double radius, int i, int j);
+
 
 #endif //_DIFFUSIONMATRIX_H
