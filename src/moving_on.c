3a63315310b73577b35e185f6dc624b374bc644f
Michael O'Donnell <mo14776@my.bristol.ac.uk>
2017-10-17 13:53:00 +0100

    Creation of moving on routines files

---
+++ src/moving_on.c
@@ -0,0 +1,20 @@
+
+/*********************
+* Date of creation 17/10/2017
+* Author: MichaelO'Donnell
+* Contact: mo14776@my.bristol.ac.uk
+* Other Authors: N/A
+**************************************
+* History
+**************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include "moving_on.h"
+
+void moving_on_routine(int numberOfParticles, double timeStep, double *diffusionmatrix, double *additionalForces, double *stochasticForce, double *generalisedCoordinates)
+{
+    
+}
