25b785f22060f4266def851caf63e76fa48f2873
Michael O'Donnell <mo14776@my.bristol.ac.uk>
2018-03-20 19:46:44 +0000

    MPI ttesting

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 **************************************
 * Change History
 **************************************/
+#define MASTER 0
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,6 +16,7 @@
 #include <gsl/gsl_rng.h>
 #include <omp.h>
 
+#include "mpi.h"
 #include "particles.h"
 #include "diffusionmatrix.h"
 #include "stochastic_force.h"
@@ -30,21 +32,29 @@ double gGrav = 9.80665; // m s^-2
 int gDebug = 0;
 int gSerial = 0;
 int gNumOfthreads;
+int gNumOfNodes;
 
 int main(int argc, char *argv[])
 {
-
+    int MPI_error = 0;
 	int numberOfParticles = 0;
-	
+
 	double xMax = 1E-7;
 	double yMax = 1E-7;
 	double zMax = 1E-7;
-	
+
 
 	//
 	// Allocate the environmental conditions and nano particle
 	// characteristics
 	//
+	MPI_Status status;
+	MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &gNumOfNodes);
+
+	int taskid;
+    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+
 
 	environmentVariables conditions;
 	conditions.temperature = 298; // K
@@ -74,7 +84,7 @@ int main(int argc, char *argv[])
 					return -1;
 				}
 			}
-      else if (strstr(argv[i],"-numthreads") != NULL)
+      		else if (strstr(argv[i],"-numthreads") != NULL)
 			{
 				if (sscanf(argv[i+1],"%d", &gNumOfthreads) != 1)
 				{
@@ -170,299 +180,302 @@ int main(int argc, char *argv[])
 		else if (gDebug == 0 && gSerial == 1) printf("Serial mode active\n");
 
 	}
+
 	omp_set_num_threads(gNumOfthreads);
-
-    FILE *output = fopen("../bin/output.csv","w");
-    FILE *angle_output = fopen("../bin/angle_output.csv","w");
-	FILE *forces_output = fopen("../bin/forces_output.csv","w");
-    if(output == NULL)
-    {
-        printf("-Error %d : %s\n : File %s : Line : %d", errno, strerror( errno ), __FILE__, __LINE__);
-        return -errno;
-    }
-
-	/*time_t tSeed1;
-	time(&tSeed1);
-	long int tSeed = -1*(long int) tSeed1;*/
-
-	gsl_rng **rndarray = calloc(gNumOfthreads,sizeof(*rndarray));
-
-	for(int i = 0; i<gNumOfthreads; i++)
+	if(MASTER == 0)
 	{
-		rndarray[i] = gsl_rng_alloc(gsl_rng_mt19937);
-		gsl_rng_set(rndarray[i], i);
-	}
-
-	gsl_rng *tSeed = gsl_rng_alloc(gsl_rng_mt19937);
-
-	particleVariables* particles = NULL;
-
-	if (numberOfParticles == 0)
-	{
-	    //
-	    // Call function to read in particle data
-	    //
-	    if( (numberOfParticles = particle_read_in(&particles)) <= 0)
+	    FILE *output = fopen("../bin/output.csv","w");
+	    FILE *angle_output = fopen("../bin/angle_output.csv","w");
+		FILE *forces_output = fopen("../bin/forces_output.csv","w");
+	    if(output == NULL)
 	    {
-	        getchar();
-	        return numberOfParticles;
+	        printf("-Error %d : %s\n : File %s : Line : %d", errno, strerror( errno ), __FILE__, __LINE__);
+	        return -errno;
 	    }
 
-	    printf("Data read in success\n" );
-	}
-	else
-	{
-		generate_particle_data(numberOfParticles, &particles, tSeed, xMax, yMax, zMax);
+		/*time_t tSeed1;
+		time(&tSeed1);
+		long int tSeed = -1*(long int) tSeed1;*/
 
-	//	printf("Generated particle data\n");
-	}
+		gsl_rng **rndarray = calloc(gNumOfthreads,sizeof(*rndarray));
 
-	// Create driving field
-	field_t drivingField;
-	drivingField.mag = 1E-10;
-	drivingField.alpha = 0;
-	drivingField.beta = gPi/2;
+		for(int i = 0; i<gNumOfthreads; i++)
+		{
+			rndarray[i] = gsl_rng_alloc(gsl_rng_mt19937);
+			gsl_rng_set(rndarray[i], i);
+		}
 
-    //
-    // Create generalised coordinates
-    //
+		gsl_rng *tSeed = gsl_rng_alloc(gsl_rng_mt19937);
 
-    double *generalisedCoordinates = NULL ;
+		particleVariables* particles = NULL;
 
-    if( (generalisedCoordinates = generalised_coordinate_creation( numberOfParticles, particles) ) == NULL )
-    {
-        free( particles );
-        particles = NULL ;
-        getchar();
-        return errno;
-    }
+		if (numberOfParticles == 0)
+		{
+		    //
+		    // Call function to read in particle data
+		    //
+		    if( (numberOfParticles = particle_read_in(&particles)) <= 0)
+		    {
+		        getchar();
+		        return numberOfParticles;
+		    }
 
-    //---------------------------- DEBUG------------------------------//
-    //
-    // Prints the generalisedCoordinates to a file for inspection
-    //
-    if( gDebug == 1 && generalisedCoordinates != NULL)
-    {
-        FILE *genCoordOutput = fopen("../bin/genCoord_output.txt","w");
+		    printf("Data read in success\n" );
+		}
+		else
+		{
+			generate_particle_data(numberOfParticles, &particles, tSeed, xMax, yMax, zMax);
 
-        for(int i = 0; i < 6 * numberOfParticles; i++)
-        {
-            fprintf(genCoordOutput, "%e\n", generalisedCoordinates[i]);
-        }
-        fclose (genCoordOutput);
-    }
-    //---------------------------END---------------------------------//
+		//	printf("Generated particle data\n");
+		}
 
+		// Create driving field
+		field_t drivingField;
+		drivingField.mag = 1E-10;
+		drivingField.alpha = 0;
+		drivingField.beta = gPi/2;
 
-    //
-    // Allocate memory required for the program.
-    // Requires: Diffusion matrix, stochastic displacement,
-    //          additional forces, stochasticWeighting,
-    //          velocities
-    //
+	    //
+	    // Create generalised coordinates
+	    //
 
+	    double *generalisedCoordinates = NULL ;
 
-    double *diffusionMatrix = NULL ;
-    double *stochasticWeighting = NULL;
-    double *additionalForces = NULL;
-    double *stochasticDisplacement = NULL;
-
-    diffusionMatrix = calloc( pow( 6 * numberOfParticles, 2), sizeof *diffusionMatrix) ;
-    stochasticWeighting = calloc( pow( 6 * numberOfParticles, 2), sizeof *stochasticWeighting);
-    stochasticDisplacement = calloc( 6 * numberOfParticles, sizeof *stochasticDisplacement);
-    additionalForces = calloc( 6 * numberOfParticles, sizeof *additionalForces);
-
-    if(  diffusionMatrix==NULL  || stochasticWeighting==NULL || stochasticDisplacement==NULL || additionalForces==NULL)
-    {
-        free( particles );
-        particles = NULL ;
-
-        free( generalisedCoordinates );
-        generalisedCoordinates = NULL ;
-
-        if( diffusionMatrix!=NULL)
-        free( diffusionMatrix );
-        diffusionMatrix = NULL;
-
-        if( stochasticDisplacement!=NULL)
-        free( stochasticDisplacement );
-        stochasticDisplacement = NULL;
-
-        if( stochasticWeighting!=NULL)
-        free( stochasticWeighting );
-        stochasticWeighting = NULL;
-
-        if( additionalForces!=NULL)
-        free( additionalForces );
-        additionalForces = NULL;
-
-
-        printf("-Error %d : %s\n : File %s : Line : %d", errno, strerror( errno ), __FILE__, __LINE__);
-
-        return -errno;
-    }
-
-    //
-    //  Choose forces to be included
-    //
-    int numberOfForces = 2; // must be at least 1, with the force none chosen
-    //
-    // Copy of enum to understand force forceList
-    //
-    //enum forces_available
-    //{
-    //    NONE ,
-    //    GRAVITY ,
-    //    VAN_DER_WAALS ,
-    //    EXP_REPULSION ,
-    //	  ALIGN_TORQUE ,
-    //	  DRIVING_FIELD,
-	  //	  POLAR_DRIVING_FORCE
-    //};
-
-
-    int forceList[4] = {VAN_DER_WAALS,EXP_REPULSION, POLAR_DRIVING_FORCE, ALIGN_TORQUE};
-
-    //
-    // Loop through time, output each time step to a file.
-    //
-    int loop = 0;
-    int count = 0;
-    int maxLoop = conditions.endTime/(double)conditions.deltaTime;
-
-    double progTime = omp_get_wtime();
-
-    while(conditions.currentTime<=conditions.endTime)
-    {
-        //
-        // Create diffusion matrix
-        //
-
-	    diffusion_matrix_creation( numberOfParticles, diffusionMatrix, stochasticWeighting, generalisedCoordinates, &conditions);
+	    if( (generalisedCoordinates = generalised_coordinate_creation( numberOfParticles, particles) ) == NULL )
+	    {
+	        free( particles );
+	        particles = NULL ;
+	        getchar();
+	        return errno;
+	    }
 
 	    //---------------------------- DEBUG------------------------------//
 	    //
-	    // Prints the diffusionMatrix to a file for inspection
+	    // Prints the generalisedCoordinates to a file for inspection
 	    //
-	    if( gDebug == 1 && diffusionMatrix != NULL)
+	    if( gDebug == 1 && generalisedCoordinates != NULL)
 	    {
-	        //conditions.currentTime = conditions.endTime+1;
-	        FILE *matrixOutput = fopen("../bin/matrix_output.txt","w");
+	        FILE *genCoordOutput = fopen("../bin/genCoord_output.txt","w");
 
 	        for(int i = 0; i < 6 * numberOfParticles; i++)
 	        {
-	            for(int j = 0; j < 6 * numberOfParticles; j++)
-	            {
-	                fprintf(matrixOutput, "%e\t", diffusionMatrix[i * 6 * numberOfParticles + j]);
-	            }
-	            fprintf(matrixOutput, "\n");
-
+	            fprintf(genCoordOutput, "%e\n", generalisedCoordinates[i]);
 	        }
-	        fclose (matrixOutput);
+	        fclose (genCoordOutput);
 	    }
 	    //---------------------------END---------------------------------//
 
 
 	    //
-	    // Create the stochastic displacement
+	    // Allocate memory required for the program.
+	    // Requires: Diffusion matrix, stochastic displacement,
+	    //          additional forces, stochasticWeighting,
+	    //          velocities
 	    //
 
-	    stochastic_displacement_creation( numberOfParticles, stochasticWeighting, stochasticDisplacement, rndarray, conditions.deltaTime);
 
-		if( gDebug == 1 && stochasticWeighting != NULL)
+	    double *diffusionMatrix = NULL ;
+	    double *stochasticWeighting = NULL;
+	    double *additionalForces = NULL;
+	    double *stochasticDisplacement = NULL;
+
+	    diffusionMatrix = calloc( pow( 6 * numberOfParticles, 2), sizeof *diffusionMatrix) ;
+	    stochasticWeighting = calloc( pow( 6 * numberOfParticles, 2), sizeof *stochasticWeighting);
+	    stochasticDisplacement = calloc( 6 * numberOfParticles, sizeof *stochasticDisplacement);
+	    additionalForces = calloc( 6 * numberOfParticles, sizeof *additionalForces);
+
+	    if(  diffusionMatrix==NULL  || stochasticWeighting==NULL || stochasticDisplacement==NULL || additionalForces==NULL)
 	    {
-	    	//conditions.currentTime = conditions.endTime+1;
-	        FILE *stochasticOutput = fopen("../bin/stochastic_matrix_output.txt","w");
+	        free( particles );
+	        particles = NULL ;
 
-	        for(int i = 0; i < 6 * numberOfParticles; i++)
+	        free( generalisedCoordinates );
+	        generalisedCoordinates = NULL ;
+
+	        if( diffusionMatrix!=NULL)
+	        free( diffusionMatrix );
+	        diffusionMatrix = NULL;
+
+	        if( stochasticDisplacement!=NULL)
+	        free( stochasticDisplacement );
+	        stochasticDisplacement = NULL;
+
+	        if( stochasticWeighting!=NULL)
+	        free( stochasticWeighting );
+	        stochasticWeighting = NULL;
+
+	        if( additionalForces!=NULL)
+	        free( additionalForces );
+	        additionalForces = NULL;
+
+
+	        printf("-Error %d : %s\n : File %s : Line : %d", errno, strerror( errno ), __FILE__, __LINE__);
+
+	        return -errno;
+	    }
+
+	    //
+	    //  Choose forces to be included
+	    //
+	    int numberOfForces = 2; // must be at least 1, with the force none chosen
+	    //
+	    // Copy of enum to understand force forceList
+	    //
+	    //enum forces_available
+	    //{
+	    //    NONE ,
+	    //    GRAVITY ,
+	    //    VAN_DER_WAALS ,
+	    //    EXP_REPULSION ,
+	    //	  ALIGN_TORQUE ,
+	    //	  DRIVING_FIELD,
+		  //	  POLAR_DRIVING_FORCE
+	    //};
+
+
+	    int forceList[4] = {VAN_DER_WAALS,EXP_REPULSION, POLAR_DRIVING_FORCE, ALIGN_TORQUE};
+
+	    //
+	    // Loop through time, output each time step to a file.
+	    //
+	    int loop = 0;
+	    int count = 0;
+	    int maxLoop = conditions.endTime/(double)conditions.deltaTime;
+
+	    double progTime = omp_get_wtime();
+
+	    while(conditions.currentTime<=conditions.endTime)
+	    {
+	        //
+	        // Create diffusion matrix
+	        //
+
+		    diffusion_matrix_creation( numberOfParticles, diffusionMatrix, stochasticWeighting, generalisedCoordinates, &conditions);
+
+		    //---------------------------- DEBUG------------------------------//
+		    //
+		    // Prints the diffusionMatrix to a file for inspection
+		    //
+		    if( gDebug == 1 && diffusionMatrix != NULL)
+		    {
+		        //conditions.currentTime = conditions.endTime+1;
+		        FILE *matrixOutput = fopen("../bin/matrix_output.txt","w");
+
+		        for(int i = 0; i < 6 * numberOfParticles; i++)
+		        {
+		            for(int j = 0; j < 6 * numberOfParticles; j++)
+		            {
+		                fprintf(matrixOutput, "%e\t", diffusionMatrix[i * 6 * numberOfParticles + j]);
+		            }
+		            fprintf(matrixOutput, "\n");
+
+		        }
+		        fclose (matrixOutput);
+		    }
+		    //---------------------------END---------------------------------//
+
+
+		    //
+		    // Create the stochastic displacement
+		    //
+
+		    stochastic_displacement_creation( numberOfParticles, stochasticWeighting, stochasticDisplacement, rndarray, conditions.deltaTime);
+
+			if( gDebug == 1 && stochasticWeighting != NULL)
+		    {
+		    	//conditions.currentTime = conditions.endTime+1;
+		        FILE *stochasticOutput = fopen("../bin/stochastic_matrix_output.txt","w");
+
+		        for(int i = 0; i < 6 * numberOfParticles; i++)
+		        {
+		            for(int j = 0; j < 6 * numberOfParticles; j++)
+		            {
+		                fprintf(stochasticOutput, "%e\t", stochasticWeighting[i * 6 * numberOfParticles + j]);
+		            }
+		            fprintf(stochasticOutput, "\n");
+		        }
+		        fclose (stochasticOutput);
+			}
+
+
+			//
+			// Include additional forces
+			//
+
+	    	force_torque_summation(additionalForces, generalisedCoordinates, 6 * numberOfParticles, forceList, numberOfForces, conditions, drivingField);
+
+
+	        //
+	        // Calculate time step.
+	        //
+	        moving_on_routine(numberOfParticles, &conditions, diffusionMatrix, additionalForces, stochasticDisplacement, generalisedCoordinates, NULL);
+	        if(loop%100 == 0)
 	        {
-	            for(int j = 0; j < 6 * numberOfParticles; j++)
+				int angle_offset = 3*numberOfParticles;
+	            fprintf(output, "%e, ", conditions.currentTime);
+	            fprintf(angle_output, "%e, ", conditions.currentTime);
+				fprintf(forces_output, "%e,",conditions.currentTime);
+	            for(int i = 0; i < 3 * numberOfParticles; i++)
 	            {
-	                fprintf(stochasticOutput, "%e\t", stochasticWeighting[i * 6 * numberOfParticles + j]);
+	                fprintf(output, "%e", generalisedCoordinates[i]);
+					fprintf(forces_output, "%e", additionalForces[i]);
+	                fprintf(angle_output, "%e", fmod(generalisedCoordinates[angle_offset + i],2*gPi));
+					if (i < 3*numberOfParticles - 1)
+		                fprintf(output, ", ");
+		                fprintf(angle_output, ", ");
+						fprintf(forces_output, ", ");
 	            }
-	            fprintf(stochasticOutput, "\n");
+	            fprintf(output, "\n");
+	            fprintf(angle_output, "\n");
+				fprintf(forces_output, "\n");
 	        }
-	        fclose (stochasticOutput);
-		}
 
+			loop ++;
+	        conditions.currentTime+=conditions.deltaTime; // time step
+			if((maxLoop/10)*count == loop)
+			{
+				//printf("%d%%\n",count*10);
+				count++;
+			}
+	        loop ++;
+	    }
+		progTime = omp_get_wtime() - progTime;
 
-		//
-		// Include additional forces
-		//
+		printf("Run time %lf s\n",progTime);
 
-    	force_torque_summation(additionalForces, generalisedCoordinates, 6 * numberOfParticles, forceList, numberOfForces, conditions, drivingField);
+	    //
+	    // Free memory
+	    //
 
+	    fclose (output);
+		fclose(angle_output);
+		fclose(forces_output);
 
-        //
-        // Calculate time step.
-        //
-        moving_on_routine(numberOfParticles, &conditions, diffusionMatrix, additionalForces, stochasticDisplacement, generalisedCoordinates, NULL);
-        if(loop%100 == 0)
-        {
-			int angle_offset = 3*numberOfParticles;
-            fprintf(output, "%e, ", conditions.currentTime);
-            fprintf(angle_output, "%e, ", conditions.currentTime);
-			fprintf(forces_output, "%e,",conditions.currentTime);
-            for(int i = 0; i < 3 * numberOfParticles; i++)
-            {
-                fprintf(output, "%e", generalisedCoordinates[i]);
-				fprintf(forces_output, "%e", additionalForces[i]);
-                fprintf(angle_output, "%e", fmod(generalisedCoordinates[angle_offset + i],2*gPi));
-				if (i < 3*numberOfParticles - 1)
-	                fprintf(output, ", ");
-	                fprintf(angle_output, ", ");
-					fprintf(forces_output, ", ");
-            }
-            fprintf(output, "\n");
-            fprintf(angle_output, "\n");
-			fprintf(forces_output, "\n");
-        }
-		
-		loop ++;
-        conditions.currentTime+=conditions.deltaTime; // time step
-		if((maxLoop/10)*count == loop)
-		{
-			//printf("%d%%\n",count*10);
-			count++;
-		}
-        loop ++;
-    }
-	progTime = omp_get_wtime() - progTime;
-
-	printf("Run time %lf s\n",progTime);
-
-    //
-    // Free memory
-    //
-
-    fclose (output);
-	fclose(angle_output);
-	fclose(forces_output);
-
-    if( particles != NULL )
-    {
-        free( particles );
-        particles = NULL;
-    }
-    if( diffusionMatrix != NULL )
-    {
-        free( diffusionMatrix );
-        particles = NULL;
-    }
-    if( generalisedCoordinates != NULL )
-    {
-        free( generalisedCoordinates );
-        particles = NULL;
-    }
-    if( stochasticWeighting != NULL )
-    {
-        free( stochasticWeighting );
-        particles = NULL;
-    }
-    if( stochasticDisplacement != NULL )
-    {
-        free( stochasticDisplacement );
-        particles = NULL;
-    }
+	    if( particles != NULL )
+	    {
+	        free( particles );
+	        particles = NULL;
+	    }
+	    if( diffusionMatrix != NULL )
+	    {
+	        free( diffusionMatrix );
+	        particles = NULL;
+	    }
+	    if( generalisedCoordinates != NULL )
+	    {
+	        free( generalisedCoordinates );
+	        particles = NULL;
+	    }
+	    if( stochasticWeighting != NULL )
+	    {
+	        free( stochasticWeighting );
+	        particles = NULL;
+	    }
+	    if( stochasticDisplacement != NULL )
+	    {
+	        free( stochasticDisplacement );
+	        particles = NULL;
+	    }
+	}
     return 0;
 }
