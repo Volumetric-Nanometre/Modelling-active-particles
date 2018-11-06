8f76ce4f6b5054bc5264e29a0f2d659d30918e1b
Michael O'Donnell <mo14776@my.bristol.ac.uk>
2018-11-06 12:37:49 +0000

    Fix issue #50

--- include/stochastic_force.h
+++ include/stochastic_force.h
@@ -15,6 +15,6 @@
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_rng.h>
 
-void stochastic_displacement_creation(int numberOfParticles, double *stochasticWeighting, double *stochasticDisplacement, gsl_rng *rndarray[], double timestep);
+void stochastic_displacement_creation(int numberOfParticles, double *stochasticWeighting, double *stochasticDisplacement, gsl_rng *rndarray[],double *rndNumArray, double timestep);
 
 #endif // _STOCHASTIC_FORCE_H
